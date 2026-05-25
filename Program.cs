0627fc2dd1a4782df7a8e466df2bfe1a4f6768c8
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-05-25 20:54:38 -0300

    Ejercicio 1 completado

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+Console.WriteLine("Hello, World!");
 
 int a;
 
@@ -11,3 +12,33 @@ b = a;
 Console.WriteLine("valor de a: "+a);
 
 Console.WriteLine("valor de b: "+b);
+*/
+
+int numeroEntero;
+string numero;
+
+do
+{
+    Console.WriteLine("Ingrese un numero: ");
+    numero = Console.ReadLine();
+} while (!(int.TryParse(numero, out numeroEntero)));
+
+
+if (numeroEntero > 0)
+{
+    int numeroInvertido = 0;
+    int aux;
+
+    while (numeroEntero > 0)
+    {
+        aux = numeroEntero % 10;
+        numeroEntero = numeroEntero / 10;    
+        numeroInvertido = numeroInvertido * 10 + aux;
+    }
+
+    Console.WriteLine($"El numero ingresado es: {numeroEntero}");
+    Console.WriteLine($"El numero invertido es: {numeroInvertido}");
+} else
+{
+    Console.WriteLine($"El numero ingresado debe ser mayor a 0 para ser invertido");
+}
